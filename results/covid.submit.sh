d932619935b284a981e31ecd804208cdb7034344
davidyuyuan <davidyuan@ebi.ac.uk>
2021-11-29 17:02:35 +0000

    Pulling results from GCS and generating output.tar.gz.

---
+++ results/covid.submit.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Umbrella study for all COVID-19 submissions: https://www.ebi.ac.uk/ena/browser/view/PRJEB45555?show=component-projects
+# Specific project for output.tar.gz: https://www.ebi.ac.uk/ena/browser/view/PRJEB43947
+
+home_dir=${1:-'/mnt/result/from_gcs/nanopore/new'}
+cd "${home_dir}" || exit
+#home_dir='/Users/davidyuan/IdeaProjects/davidyuyuan/ENA_SARS_Cov2_nanopore/results/new'
+#cd "${home_dir}" || exit
+#scp -i ~/.ssh/david-yuan-tsi.pem centos@45.88.81.197:/mnt/result/from_gcs/nanopore/new/ERR4080473* ${home_dir}
+#scp -i ~/.ssh/david-yuan-tsi.pem centos@45.88.81.197:/mnt/result/from_gcs/nanopore/new/ERR4080474* ${home_dir}
+#scp -i ~/.ssh/david-yuan-tsi.pem centos@45.88.81.197:/mnt/result/from_gcs/nanopore/new/ERR4080475* ${home_dir}
+
+for filename in $(basename "${home_dir}"/*)
+do
+  run_accession=$(echo "${filename}" | cut -d '.' -f 1 | cut -d '_' -f 1)
+  mkdir -p "${home_dir}/${run_accession}_output"
+
+  case "${filename}" in
+  *.vcf | *.coverage )
+    gzip "${home_dir}/${filename}"
+    mv "${home_dir}/${filename}.gz" "${home_dir}/${run_accession}_output"
+    ;;
+  * )
+    mv "${home_dir}/${filename}" "${home_dir}/${run_accession}_output"
+    ;;
+  esac
+done
+
+staging_dir="${home_dir}/../staging" && mkdir -p ${staging_dir}
+for filename in $(basename "${home_dir}"/*)
+do
+  run_accession=$(echo "${filename}" | cut -d '_' -f 1)
+  output_tgz=${run_accession}_output.tar.gz
+  tar -zcvf "${home_dir}/${output_tgz}" "${run_accession}_output" && rm -R "${run_accession}_output" && mv "${home_dir}/${output_tgz}" "${staging_dir}"
+#  printf '%s\n' "${run_accession} ${filename}"
+done
---
+++ results/cp.gcs.ec.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# DIR where the current script resides
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install Minoconda with Python 3.9
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
+# Install Google Cloud SSDK
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo dnf install google-cloud-sdk
+gcloud init
+
+# Attach and mount Cinder dy-nextflow: 8 TiB SSD
+lsblk
+sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/vdb
+sudo mkdir -p /mnt/result
+sudo mount -o discard,defaults /dev/vdb /mnt/result
+sudo chmod a+w /mnt/result
+sudo blkid /dev/vdb
+sudo vi /etc/fstab
+#UUID=5564fd53-d902-4124-8187-70c25c469251 /mnt/result ext4 discard,defaults 0 2
+
+# Pull new results and duplicated results separately
+nohhup gsutil -m cp -n -I /mnt/result/from_gcs/nanopore/new < /mnt/result/from_gcs/nanopore/new_results.txt
+nohhup gsutil -m cp -n -I /mnt/result/from_gcs/nanopore/duplicated < /mnt/result/from_gcs/nanopore/duplicated_results.txt
+
+
+##!/usr/bin/env bash
+#
+#export AWS_ACCESS_KEY_ID=D0FVX5OMT6UWFNN5DVFA
+#export AWS_SECRET_ACCESS_KEY=GwIROUcKvad2bjhuEbVu6TNQr7kPToeisrPABk3Z
+#export AWS_DEFAULT_REGION=us-east-1
+#
+## DIR where the current script resides
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#
+#while IFS="" read -r p || [ -n "$p" ]
+#do
+##   printf '%s\n' "$p"
+##   printf '%s\n' $(basename "$p")
+#    gsutil -m cp "$p" "${DIR}"
+#    aws s3 --endpoint-url https://uk1s3.embassy.ebi.ac.uk cp $(basename "$p") s3://results_from_gcs
+#    rm $(basename "$p")
+#    # ls -ltr
+#    # aws s3 --endpoint-url https://uk1s3.embassy.ebi.ac.uk ls s3://results_from_gcs
+#done < "${DIR}/short_results.txt"
