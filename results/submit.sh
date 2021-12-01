5e5b301d24ae04dc6458cfca7ef6e6696de3983b
davidyuyuan <davidyuan@ebi.ac.uk>
2021-12-01 10:53:57 +0000

    Test submit.sh on Embassy node.

--- results/submit.sh
+++ results/submit.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 
-#########################################
-# TODO! To be merged into covid.submit.sh
-#########################################
-
-home_dir=${1:-'/mnt/result/from_gcs/nanopore/new'}
+staging_dir=${1:-'/mnt/result/from_gcs/nanopore/staging'}
 date_submitted=${2:-'2021-11-29'}
-cd "${home_dir}" || exit
+cd "${staging_dir}" || exit
+#staging_dir='/Users/davidyuan/IdeaProjects/davidyuyuan/ENA_SARS_Cov2_nanopore/results/staging'
 #date_submitted='2021-11-29'
-#home_dir='/Users/davidyuan/IdeaProjects/davidyuyuan/ENA_SARS_Cov2_nanopore/results/new'
-#cd "${home_dir}" || exit
+#cd "${staging_dir}" || exit
 
-staging_dir="${home_dir}/../staging"
-##############
-# Additional #
-##############
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
